d5ad930a616037d00e93fdda5753a10db8773005
dcalixto <calixtodaniel@gmail.com>
2024-12-02 05:00:36 -0300

    set constants and macro-time evaluation to handle the slug base configuration.

--- src/friendly_id/slugged.cr
+++ src/friendly_id/slugged.cr
@@ -1,17 +1,19 @@
 module FriendlyId
   module Slugged
     macro included
-      class_property slug_base : String = "title"
+      SLUG_BASE = "title"
       property slug : String? = nil
       @slug_changed : Bool = false
       @previous_value : String? = nil
       
       def self.slug_from(field_name : String)
-        self.slug_base = field_name
+        {% if field_name %}
+          SLUG_BASE = field_name
+        {% end %}
       end
 
       def generate_slug
-        source_value = self.responds_to?(self.class.slug_base) ? self.send(self.class.slug_base) : ""
+        source_value = self.responds_to?(SLUG_BASE) ? self.send(SLUG_BASE) : ""
         if @previous_value != source_value
           self.slug = normalize_friendly_id(source_value.to_s)
           @slug_changed = true
