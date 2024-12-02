9d2fbe91fc3e2a6f56f9c8d8b95f0fc01e7bf379
dcalixto <calixtodaniel@gmail.com>
2024-12-02 18:34:01 -0300

    Define table_name in the Finders Module fix

--- src/friendly_id/finders.cr
+++ src/friendly_id/finders.cr
@@ -1,10 +1,13 @@
 module FriendlyId
   module Finders
     macro included
-      @@table_name : String = {{@type.stringify.downcase.split("::").last + "s"}}
+      # Define a class-level method to return the table name
+      def self.table_name
+        {{@type.stringify.split("::").last.downcase.id + "s"}}
+      end
 
       def self.find_by_friendly_id(slug_or_id : String)
-        # First try to find by slug
+        # First, try to find by slug
         if record = find_by_slug(slug_or_id)
           return record
         end
@@ -14,33 +17,38 @@ module FriendlyId
           find_by_id(slug_or_id.to_i64)
         end
       end
-      
-     # private def self.find_by_slug(slug : String)
-     #   query = "SELECT * FROM #{@@table_name} WHERE slug = ?"
-     #  @@db.query_one?(query, slug, as: self)
-     # end
-
 
       def self.find_by_slug(slug : String)
-        # First try current slugs
-        result = @@db.query_one?(
-          "SELECT * FROM #{table_name} WHERE slug = ?", 
-          slug, 
-          as: self
-        )
-        
-        # If not found, try historical slugs
-        if result.nil?
-          if historical_record = FriendlyId::Slug.find_by_slug(slug)
-            result = find(historical_record.sluggable_id)
+        begin
+          # First try current slugs
+          result = @@db.query_one?(
+            "SELECT * FROM #{table_name} WHERE slug = ?", 
+            slug, 
+            as: self
+          )
+
+          # If not found, try historical slugs
+          if result.nil?
+            if historical_record = FriendlyId::Slug.find_by_slug(slug, @@db)
+              result = find(historical_record.sluggable_id)
+            end
           end
+
+          result
+        rescue ex : DB::Error
+          puts "Database error during find_by_slug: #{ex.message}"
+          nil
         end
-        
-        result
       end
+
       private def self.find_by_id(id : Int64)
-        query = "SELECT * FROM #{@@table_name} WHERE id = ?"
-        @@db.query_one?(query, id, as: self)
+        begin
+          query = "SELECT * FROM #{table_name} WHERE id = ?"
+          @@db.query_one?(query, id, as: self)
+        rescue ex : DB::Error
+          puts "Database error during find_by_id: #{ex.message}"
+          nil
+        end
       end
     end
   end
