9a4c2ed37eaa755f307116ed8bf6fb6435a8756d
dcalixto <calixtodaniel@gmail.com>
2024-12-02 05:23:13 -0300

    changed macro

--- src/friendly_id/slugged.cr
+++ src/friendly_id/slugged.cr
@@ -1,11 +1,11 @@
 module FriendlyId
   module Slugged
+    # Main include macro remains the same
     macro included
       property slug : String? = nil
       @slug_changed : Bool = false
       @previous_value : String? = nil
-      
-      # Default slug generation from title
+
       def generate_slug
         source_value = self.title
         if @previous_value != source_value
@@ -15,21 +15,21 @@ module FriendlyId
         end
       end
 
-      # Macro for custom field selection
-      macro slug_from(field_name)
-        def generate_slug
-          source_value = self.{{field_name.id}}
-          if @previous_value != source_value
-            self.slug = normalize_friendly_id(source_value.to_s)
-            @slug_changed = true
-            @previous_value = source_value
-          end
-        end
-      end
-
       def save
         generate_slug
         super
+      end
+    end
+
+    # New macro to override slug source
+    macro friendly_id(field)
+      def generate_slug
+        source_value = self.{{field.id}}
+        if @previous_value != source_value
+          self.slug = normalize_friendly_id(source_value.to_s)
+          @slug_changed = true
+          @previous_value = source_value
+        end
       end
     end
 
