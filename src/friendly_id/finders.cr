8518881fe501790a3d4fca877055b4d8ef55e90e
dcalixto <calixtodaniel@gmail.com>
2024-12-02 13:51:09 -0300

    add the table_name class variable to make the finder work correctly.

--- src/friendly_id/finders.cr
+++ src/friendly_id/finders.cr
@@ -1,6 +1,8 @@
 module FriendlyId
   module Finders
     macro included
+      @@table_name : String = {{@type.stringify.downcase.split("::").last + "s"}}
+
       def self.find_by_friendly_id(slug : String)
         query = "SELECT * FROM #{@@table_name} WHERE slug = ?"
         @@db.query_one?(query, slug, as: self)
