532d240d0acc4a3c0a46c09d8869c64d1e77a43b
dcalixto <calixtodaniel@gmail.com>
2024-12-02 04:04:51 -0300

    added config due someone have custom db migration path

--- src/friendly_id.cr
+++ src/friendly_id.cr
@@ -1,4 +1,17 @@
 require "./friendly_id/*"
 
 module FriendlyId
+  class Configuration
+    property migration_dir : String = "db/migrations"
+  end
+
+  @@configuration = Configuration.new
+
+  def self.configure
+    yield @@configuration
+  end
+
+  def self.config
+    @@configuration
+  end
 end
