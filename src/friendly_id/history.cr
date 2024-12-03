390634a481ac9d6e13ccfdf0a940808282ee20b6
dcalixto <calixtodaniel@gmail.com>
2024-12-02 23:38:11 -0300

    Updated the slug_history array directly

--- src/friendly_id/history.cr
+++ src/friendly_id/history.cr
@@ -6,19 +6,17 @@ module FriendlyId
     end
 
     def slugs
-      raise "ID is missing for #{self.class.name}" unless id
-
+      return [] of String if id.nil?
       FriendlyId::Slug.where({sluggable_id: id.not_nil!, sluggable_type: self.class.name})
     end
 
     def slug_history
-      raise "ID is missing for #{self.class.name}" unless id
-
-      FriendlyId::Slug.where({sluggable_id: id.not_nil!, sluggable_type: self.class.name})
-        .map(&.slug)
+      return [] of String if id.nil?
+      slugs.map(&.slug)
     end
 
     private def create_slug_record
+      return if id.nil?
       old_slug = slug_was
       return if old_slug.nil? || slug_history.includes?(old_slug)
 
@@ -27,13 +25,16 @@ module FriendlyId
         sluggable_id: id.not_nil!,
         sluggable_type: self.class.name
       )
+      @slug_history << old_slug
     end
 
     private def update_slug_history
+      return if id.nil?
       current_slug_was = slug_was
       return unless slug != current_slug_was && current_slug_was
 
-      slug_history << current_slug_was.to_s unless slug_history.includes?(current_slug_was)
+      @slug_history << current_slug_was.to_s unless @slug_history.includes?(current_slug_was)
+      create_slug_record
     end
 
     private def slug_was
